c7ba0e0ead81420b94523f8f0da1991e361fec94
Bright-Anyawe <157108952+Bright-Anyawe@users.noreply.github.com>
2024-08-12 16:06:30 +0000

    Refactor: add function to start image slide

--- index.js
+++ index.js
@@ -57,3 +57,48 @@ const dots = document.querySelectorAll(".dot");
 const pictureFrame = document.querySelector(".picture-frame");
 const nextEl = document.querySelector(".next");
 const previousEl = document.querySelector("#previous");
+
+//Get Images into an array
+const images = [
+  { src: "asset/image/todo-list image-with-a-person.jpg", alt: "To_Do_List" },
+  { src: "asset/image/sign-up-form.jpg", alt: "sign up form" },
+  { src: "asset/image/Pf-bg.jpeg", alt: "portfolio project" },
+];
+
+let currentIndex = 0;
+let slideshowInterval;
+
+//Display current image in the picture frame
+function displayCurrentImage() {
+  pictureFrame.textContent = "";
+
+  //Create an image element and append it to the picture frame
+  let toDoImg = document.createElement("img");
+  toDoImg.classList.add("toDoImg");
+  toDoImg.src = images[currentIndex].src;
+  toDoImg.alt = images[currentIndex].alt;
+
+  pictureFrame.appendChild(toDoImg);
+
+  //Update the active dot
+  dots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === currentIndex);
+  });
+}
+displayCurrentImage();
+
+function startSlideshow() {
+  slideshowInterval = setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    displayCurrentImage();
+  }, 5000); // Change the interval time (in milliseconds) to adjust the slideshow speed
+}
+
+
+const displayNextImage = () => {
+  //Loop through the images in circular form
+  currentIndex = (currentIndex + 1) % images.length;
+  displayCurrentImage();
+  clearInterval(slideshowInterval);
+  startSlideshow();
+};
